a7c53ed2468879ca6fb26f71f8ca770b07838732
martijnkunstman <martijn@kunstman.nl>
2020-05-18 08:34:50 +0200

    refactor

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+//
+// classes
+//
 class CoronaVirus {
     public positionX: number;
     public positionY: number;
@@ -12,12 +15,12 @@ class CoronaVirus {
         this.id = "id-" + positionX + "-" + positionY + "-" + diameter + "-" + spikeCount;
     }
     create() {
-        //create virusBody
+        //draw virusBody
         document.body.innerHTML += '<div id="' + this.id + '" class="coronaVirus" style="left:' + (this.positionX - this.diameter / 2) + 'px; top:' + (this.positionY - this.diameter / 2) + 'px; width:' + this.diameter + 'px; height:' + this.diameter + 'px"></div>';
         //create virusSpikes
         for (let a = 0; a < this.spikeCount; a++) {
-            let angle = 360 / this.spikeCount * a;
-            let spike = new Spike(this.id, this.diameter, this.spikeCount, angle);
+            let angle : number  = 360 / this.spikeCount * a;
+            let spike : Spike = new Spike(this.id, this.diameter, this.spikeCount, angle);
         }
         return this.id;
     }
@@ -36,31 +39,38 @@ class Spike {
         this.create();
     }
     create() {
-        //create virusSpike
-        let posX = this.diameter / 2;
-        let posY = this.diameter / 2;
+        //draw virusSpike
+        let posX : number = this.diameter / 2;
+        let posY : number = this.diameter / 2;
 
-        let dia = this.diameter / (this.count / 2);
-        let top = posX + Math.sin(this.angle * Math.PI / 180) * (this.diameter / 2 + dia / 2) - dia / 2;
-        let left = posY + Math.cos(this.angle * Math.PI / 180) * (this.diameter / 2 + dia / 2) - dia / 2;
+        let dia : number = this.diameter / (this.count / 2);
+        let top : number = posX + Math.sin(this.angle * Math.PI / 180) * (this.diameter / 2 + dia / 2) - dia / 2;
+        let left : number = posY + Math.cos(this.angle * Math.PI / 180) * (this.diameter / 2 + dia / 2) - dia / 2;
         document.getElementById(this.id).innerHTML += '<div class="spike" style="top:' + top + 'px; left:' + left + 'px; width: ' + dia + 'px; height: ' + dia + 'px"></div>';
     }
 }
+//
+// helper functions
+//
+function getRndInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//
+// let's do it...
+//
 let coronaVirusIds : any;
 coronaVirusIds = [];
-let maxVirus = 15;
+let maxVirus : number = 15;
 function createVirus() {
-    let coronaVirus = new CoronaVirus(getRndInteger(0, window.innerWidth), getRndInteger(0, window.innerHeight), getRndInteger(50, 400), getRndInteger(6, 32));
-    let id = coronaVirus.create();
+    //create coronaVirus
+    let coronaVirus : CoronaVirus = new CoronaVirus(getRndInteger(0, window.innerWidth), getRndInteger(0, window.innerHeight), getRndInteger(50, 400), getRndInteger(6, 32));
+    let id : string = coronaVirus.create();
     coronaVirusIds.push(id);
     if (coronaVirusIds.length > maxVirus) {
-        var el = document.getElementById(coronaVirusIds[0]);
+        let el : any = document.getElementById(coronaVirusIds[0]);
         el.remove();
         coronaVirusIds.shift();
     }
     setTimeout(createVirus, 500);
 }
 createVirus();
-function getRndInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
